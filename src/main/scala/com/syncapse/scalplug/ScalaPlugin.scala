526a0181d7416082d1f271990a0247ca703f52e6
A.J. Wright <ayax79@gmail.com>
2010-05-05 13:59:29 -0700

    initial

---
+++ src/main/scala/com/syncapse/scalplug/ScalaPlugin.scala
@@ -0,0 +1,25 @@
+package com.syncapse.scalplug
+
+import com.jivesoftware.base.plugin.Plugin
+import reflect.BeanProperty
+import org.slf4j.LoggerFactory
+import com.jivesoftware.base.UserManager
+
+class ScalaPlugin extends Object with Plugin {
+
+  var log = LoggerFactory.getLogger(getClass)
+
+  @BeanProperty
+  var userManager: UserManager = null
+
+  def destroy = {
+    log.info("The plugin is now being destroyed")
+  }
+
+  def init = {
+    log.info("The plugin is now starting up")
+    val admin = userManager.getUser(1)
+    log.info("Here is the admin user "+ admin)
+  }
+  
+}
