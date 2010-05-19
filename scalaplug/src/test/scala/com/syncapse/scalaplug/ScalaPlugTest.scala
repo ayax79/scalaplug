2dc0f52229f4a0487ebb94bbb2d3c5d277f051af
A.J. Wright <ayax79@gmail.com>
2010-05-18 18:03:06 -0700

    scala test fixes.

--- scalaplug/src/test/scala/com/syncapse/scalaplug/ScalaPlugTest.scala
+++ scalaplug/src/test/scala/com/syncapse/scalaplug/ScalaPlugTest.scala
@@ -5,8 +5,8 @@ import org.specs.mock.Mockito
 import com.jivesoftware.base.{UserManager, User}
 
 class ScalaPlugTest extends SpecificationWithJUnit with Mockito {
-  var userManager = mock[UserManager]
-  var user = mock[User]
+  val userManager = mock[UserManager]
+  val user = mock[User]
 
   userManager.getUser(1) returns user
   user.getName returns "admin"
@@ -14,19 +14,20 @@ class ScalaPlugTest extends SpecificationWithJUnit with Mockito {
   var scalaPlugin = new ScalaPlugin()
   scalaPlugin.userManager = userManager
 
-  scalaPlugin.init
-  there was one(userManager).getName
 
   "The init method" should {
+    scalaPlugin.init
 
 
     "should get the admin user" in {
-      //      userManager.getUser(1) was called
+      userManager.getUser(1) was called
     }
 
     "should call the user's name method" in {
+      user.getName was called
     }
 
   }
+
 
 }
